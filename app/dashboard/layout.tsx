4aab33e55cfb0fd04c9f541df246bdb77ec8ac47
marceloglacial <glacial@gmail.com>
2024-04-05 03:36:00 +0000

    translate product

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -138,7 +138,7 @@ export default function RootLayout({
                     <Button size="sm" className="h-8 gap-1">
                       <PlusCircle className="h-3.5 w-3.5" />
                       <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                        Add Product
+                        Adicionar Novo Produto
                       </span>
                     </Button>
                   </div>
