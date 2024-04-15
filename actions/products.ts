c8e8b180e33dd00938fb8691baea1a06a780a240
marceloglacial <glacial@gmail.com>
2024-04-14 21:24:01 -0400

    format product

--- actions/products.ts
+++ actions/products.ts
@@ -1,30 +1,39 @@
 'use server'
 import { postProducts } from '@/lib/products'
-import { sanitizeEntry } from '@/lib/sanitize-entry'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
-export async function addProduct(formData: any) {
-    'use server'
-    const product = Object.fromEntries(formData)
-    product.price = {
-        regular: parseInt(formData.get('price-regular')),
-        discount: parseInt(formData.get('price-discount')),
+export async function formatEntry(obj: any) {
+    obj.price = {
+        regular: parseInt(obj['price-regular']),
+        discount: parseInt(obj['price-discount']),
     }
-    product.image = {
-        src: formData.get('file'),
+    obj.image = {
+        src: obj.file,
         alt: '',
         width: 150,
         height: 150,
     }
-    sanitizeEntry(product)
+    Object.keys(obj).forEach((key) => key.startsWith('$ACTION_ID_') && delete obj[key])
+    delete obj.file
+    delete obj['price-regular']
+    delete obj['price-discount']
+}
+
+
+export async function addProduct(formData: any) {
+    'use server'
+    const product = Object.fromEntries(formData)
+    await formatEntry(product)
     const reponse = await postProducts(product)
     revalidatePath('/admin')
     redirect(`/admin/product/${reponse.id}`)
 }
 
 export async function updateProduct(formData: any) {
-    console.log(formData)
-
+    'use server'
+    const product = Object.fromEntries(formData)
+    await formatEntry(product)
+    console.log({ product })
     return null
 }
