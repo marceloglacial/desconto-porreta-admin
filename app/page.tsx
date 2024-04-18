ffd48b1b3660d5e79fbee153471a09401e219447
marceloglacial <glacial@gmail.com>
2024-04-18 00:56:17 -0400

    add auth0

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,28 @@
+'use client'
 import Link from 'next/link'
-
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { redirect } from 'next/navigation'
 
 export default function Login() {
+    const { user, error, isLoading } = useUser();
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>{error.message}</div>;
+    if (user) return redirect('/admin')
     return (
         <div className='w-screen h-screen flex items-center justify-center'>
             <Card className='mx-auto max-w-sm'>
                 <CardHeader>
                     <CardTitle className='text-2xl'>Login</CardTitle>
-                    <CardDescription>Entre com seu email para logar na sua conta</CardDescription>
+                    <CardDescription>Entre com seu email do Google para logar na sua conta</CardDescription>
                 </CardHeader>
                 <CardContent>
                     <div className='grid gap-4'>
-                        <div className='grid gap-2'>
-                            <Label htmlFor='email'>Email</Label>
-                            <Input id='email' type='email' placeholder='m@examplo.com' required />
-                        </div>
-                        <div className='grid gap-2'>
-                            <div className='flex items-center'>
-                                <Label htmlFor='password'>Senha</Label>
-                                <Link href='#' className='ml-auto inline-block text-sm underline'>
-                                    Esqueceu sua senha?
-                                </Link>
-                            </div>
-                            <Input id='password' type='password' required />
-                        </div>
-                        <Button asChild>
-                            <a href='/admin'>Login</a>
+                        <Button variant="outline" className="w-full" asChild>
+                            <a href="/api/auth/login">Login</a>
                         </Button>
-                        {/* <Button variant="outline" className="w-full">
-              Login with Google
-            </Button> */}
                     </div>
-                    {/* <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
-              Sign up
-            </Link>
-          </div> */}
                 </CardContent>
             </Card>
         </div>
