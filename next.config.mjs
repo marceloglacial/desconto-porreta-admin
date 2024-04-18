73365abfa8b782f69c6699b51232960dfb9cf9cc
Marcelo Freitas <glacial@gmail.com>
2024-04-18 16:27:03 -0400

    Feature/login (#9)
    * add auth0
    * add loading and error states
    * small fixes

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,18 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'res.cloudinary.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: '*.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 's.gravatar.com',
+            },
         ],
     },
 }
